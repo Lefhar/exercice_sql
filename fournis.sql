8d61bc89f8ca1b22da39a426a303e4058fd6e3dc
80010-99-15 <80010-99-15@exchange.ad.afpanet>
2020-11-23 13:17:35 +0100

    exercice sql

---
+++ fournis.sql
@@ -0,0 +1,6 @@
+insert into fournis (NUMFOU, NOMFOU, RUEFOU, POSFOU, VILFOU, CONFOU, SATISF) values('8700','balladin1','1 rue de la poupée qui tousse','10235','mars','blabla1','2');
+insert into fournis (NUMFOU, NOMFOU, RUEFOU, POSFOU, VILFOU, CONFOU, SATISF) values('120','balladin2','2 rue de la poupée qui tousse','12045','mars','blabla2','4');
+insert into fournis (NUMFOU, NOMFOU, RUEFOU, POSFOU, VILFOU, CONFOU, SATISF) values('9120','balladin3','3 rue de la poupée qui tousse','12305','mars','blabla3','6');
+insert into fournis (NUMFOU, NOMFOU, RUEFOU, POSFOU, VILFOU, CONFOU, SATISF) values('9150','balladin4','4 rue de la poupée qui tousse','12346','mars','blabla4','4');
+insert into fournis (NUMFOU, NOMFOU, RUEFOU, POSFOU, VILFOU, CONFOU, SATISF) values('9180','balladin5','5 rue de la poupée qui tousse','12347','mars','blabla5','6');
+insert into fournis (NUMFOU, NOMFOU, RUEFOU, POSFOU, VILFOU, CONFOU, SATISF) values('540','balladin6','6 rue de la poupée qui tousse','12348','mars','blabla6','1');
