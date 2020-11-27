73e39843b3fa7396f1f85b11982c193ede67342e
harold <s.lefebvre907@laposte.net>
2020-11-27 12:19:02 +0100

    exercice sql

--- Papyrus-lesbesoins.sql
+++ Papyrus-lesbesoins.sql
@@ -35,5 +35,11 @@ SELECT numcom AS 'Numéro de commande', SUM(qtecde * priuni )as total FROM ligco
 -- 10 Lister les commandes par nom fournisseur (Afficher le nom du fournisseur, le numéro de commande et la date)
 SELECT nomfou AS 'Fournisseur' , entcom.numcom AS 'Numéro de commande', datcom AS 'Date' FROM ligcom join entcom on entcom.numcom = ligcom.numcom join fournis ON fournis.numfou =  entcom.numfou
 
---11 Sortir les produits des commandes ayant le mot "urgent' en observation?(Afficher le numéro de commande, le nom du fournisseur, le libellé du produit et le sous total= quantité commandée * Prix unitaire)
+-- 11 Sortir les produits des commandes ayant le mot "urgent' en observation?(Afficher le numéro de commande, le nom du fournisseur, le libellé du produit et le sous total= quantité commandée * Prix unitaire)
 SELECT entcom.numcom AS 'numéro de commande', fournis.nomfou AS 'nom du fournisseur' , produit.libart AS 'libellé du produit', SUM(qtecde * priuni) as 'Sous total'  FROM ligcom JOIN produit ON produit.codart = ligcom.codart JOIN entcom ON entcom.numcom = ligcom.numcom JOIN fournis ON fournis.numfou = entcom.numfou WHERE obscom LIKE '%urgent%' GROUP BY entcom.numcom,nomfou,libart
+
+-- 12 Coder de 2 manières différentes la requête suivante:Lister le nom des fournisseurs susceptibles de livrer au moins un article
+
+SELECT nomfou FROM ligcom join entcom on entcom.numcom = ligcom.numcom and qteliv >= 1 join fournis ON entcom.numfou = fournis.numfou GROUP BY nomfou
+--ou
+SELECT nomfou FROM entcom,fournis,ligcom WHERE entcom.numfou = fournis.numfou and entcom.numcom = ligcom.numcom  and qteliv >= 1 GROUP BY nomfou
